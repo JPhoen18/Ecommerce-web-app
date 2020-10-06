4dee93016404485fa4ca6e11d7e4ac1855c3498d
Jordan <jpa2k3@gmail.com>
2020-10-05 21:28:35 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import { gsap } from 'gsap'
-import { TimelineMax } from 'gsap/CSSPlugin'
+import { TimelineMax } from 'gsap'
 
 
 // Force TimelineMax to not get dropped during build
